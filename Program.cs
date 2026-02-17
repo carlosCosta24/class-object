d3abb3bbefb097945ae0931a9143a4e76020ee6a
carlos_costa <carloscosta.pen@gmail.com>
2026-02-17 13:58:27 -0300

    static members

--- Program.cs
+++ Program.cs
@@ -45,6 +45,22 @@ namespace class___object
             return x1 + x3;
         }
     }
+    class clsStatic {
+        public int x1;
+        //shard on the class level
+        public static int x2;
+
+        public int Method1() { 
+            //non static methods can access static members
+            return x1 + x2;
+        }
+        public static int Method2()
+        {
+            // only access a static member
+            return x2;
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -64,7 +80,7 @@ namespace class___object
             Console.WriteLine(SecondPerson.FullName());
             */
             //access modifiers exp
-
+            /*
             clsA A = new clsA();
             Console.WriteLine("Accessing public members:");
             Console.WriteLine("x1={0}", A.x1);
@@ -74,6 +90,26 @@ namespace class___object
             Console.WriteLine("\nObjects from class B expose all public members from the base class");
             Console.WriteLine("x1={0}", B.x1);
             Console.WriteLine("result of fun1={0}", B.fun1());
+            */
+
+            // static members
+
+            clsStatic FirstObj = new clsStatic();
+            clsStatic SecondObj = new clsStatic();
+
+            FirstObj.x1 = 170;
+            SecondObj.x1 = 180;
+            clsStatic.x2 = 500;
+
+            Console.WriteLine("FirstObj.x1:={0}", FirstObj.x1);
+            Console.WriteLine("SecondObj.x1:={0}", SecondObj.x1);
+            Console.WriteLine("FirstObj.method1 results:={0}", FirstObj.Method1());
+            Console.WriteLine("SecondObj.method1 results:={0}", SecondObj.Method1());
+
+            // in order to access a static method, the class name should be used
+
+            Console.WriteLine("Static method2 result = {0}", clsStatic.Method2()) ;
+            Console.WriteLine("static x2:= {0}", clsStatic.x2);
 
             Console.ReadKey();
 
